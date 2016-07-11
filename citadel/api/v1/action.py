be0acbcd9cd879355cb839070b39c12a5c46b33b
tonic <tonicbupt@gmail.com>
2016-07-11 17:58:28 +0800

    add api

--- citadel/api/v1/action.py
+++ citadel/api/v1/action.py
@@ -2,20 +2,56 @@
 
 import json
 import yaml
+from more_itertools import peekable
 from flask import abort, request, Response
+from threading import Thread
+from Queue import Queue, Empty
+from grpc.framework.interfaces.face import face
 
 from citadel.ext import core
 from citadel.libs.json import JSONEncoder
 from citadel.libs.view import create_api_blueprint
 from citadel.libs.datastructure import AbortDict
+from citadel.libs.utils import with_appcontext
 
-from citadel.models.app import App
+from citadel.models.app import App, Release
+from citadel.models.container import Container
 from citadel.models.gitlab import get_project_name, get_file_content
 
 
 # 把action都挂在/api/:version/下, 不再加前缀
 # 也不需要他帮忙自动转JSON了
 bp = create_api_blueprint('action', __name__, jsonize=False)
+
+
+_eof = object()
+
+
+def _stream_consumer(q):
+    """
+    因为grpc这边需要一直同步等待返回,
+    所以没办法啊, 只好用一个Thread来做这个事情了.
+    q就是对应的queue.
+    为什么要用Thread呢, 因为Greenlet会死... grpc好渣
+    """
+    while True:
+        try:
+            e = q.get(timeout=120)
+            if e is _eof:
+                break
+            yield e
+        except Empty:
+            break
+
+
+def _peek_grpc(call):
+    """peek一下stream的返回, 不next一次他是不会raise exception的"""
+    try:
+        ms = peekable(call)
+        ms.peek()
+    except (face.RemoteError, face.RemoteShutdownError) as e:
+        abort(400, e.details)
+    return ms
 
 
 @bp.route('/build', methods=['POST'])
@@ -26,6 +62,7 @@ def build():
     """
     data = AbortDict(request.get_json())
 
+    # TODO 参数需要类型校验
     repo = data['repo']
     sha = data['sha']
     artifact = data.get('artifact', '')
@@ -36,17 +73,164 @@ def build():
         abort(400, 'repo %s does not have app.yaml in root directory' % repo)
 
     specs = yaml.load(content)
-    if 'appname' not in specs:
-        abort(400, 'repo %s does not specify appname in app.yaml' % repo)
-
-    app = App.get_by_name(specs['appname'])
+    appname = specs.get('appname', '')
+    app = App.get_by_name(appname)
     if not app:
         abort(400, 'repo %s does not have the right appname in app.yaml' % repo)
 
     uid = data.get('uid', None) or app.id
+    ms = _peek_grpc(core.build_image(repo, sha, str(uid), artifact))
+    q = Queue()
 
-    def stream():
-        for m in core.build_image(repo, sha, str(uid), artifact):
-            yield json.dumps(m, cls=JSONEncoder) + '\n'
+    @with_appcontext
+    def _stream_producer():
+        image = ''
+        for m in ms:
+            if m.status == 'finished':
+                image = m.progress
+            q.put(json.dumps(m, cls=JSONEncoder) + '\n')
+        q.put(_eof)
 
-    return Response(stream(), mimetype='application/json')
+        release = Release.get_by_app_and_sha(appname, sha)
+        if release and image:
+            release.update_image(image)
+
+    t = Thread(target=_stream_producer)
+    t.daemon = True
+    t.start()
+
+    return Response(_stream_consumer(q), mimetype='application/json')
+
+
+@bp.route('/deploy', methods=['POST'])
+def deploy():
+    data = AbortDict(request.get_json())
+
+    # TODO 参数需要类型校验
+    repo = data['repo']
+    sha = data['sha']
+    podname = data['podname']
+    entrypoint = data['entrypoint']
+    cpu = float(data['cpu_quota'])
+    count = int(data['count'])
+    networks = data.get('networks', {})
+    env = data.get('env', [])
+
+    pod = core.get_pod(podname)
+    if not pod:
+        abort(400, 'pod %s not exist' % podname)
+
+    project_name = get_project_name(repo)
+    content = get_file_content(project_name, 'app.yaml', sha)
+    if not content:
+        abort(400, 'repo %s, %s does not have app.yaml in root directory' % (repo, sha))
+
+    specs = yaml.load(content)
+    appname = specs.get('appname', '')
+    release = Release.get_by_app_and_sha(appname, sha)
+    if not release:
+        abort(400, 'repo %s, %s does not have the right appname in app.yaml' % (repo, sha))
+
+    if not release.image:
+        abort(400, 'repo %s, %s has not been built yet' % (repo, sha))
+
+    image = release.image
+    ms = _peek_grpc(core.create_container(content, appname, image, podname, entrypoint, cpu, count, networks, env))
+    q = Queue()
+
+    @with_appcontext
+    def _stream_producer():
+        release = Release.get_by_app_and_sha(appname, sha)
+        if not release:
+            q.put(_eof)
+            return
+
+        for m in ms:
+            q.put(json.dumps(m, cls=JSONEncoder) + '\n')
+            if not m.success:
+                continue
+
+            Container.create(release.app.name, release.sha, m.id, entrypoint, 'env', cpu, m.podname, m.nodename)
+        q.put(_eof)
+
+    t = Thread(target=_stream_producer)
+    t.daemon = True
+    t.start()
+
+    return Response(_stream_consumer(q), mimetype='application/json')
+
+
+@bp.route('/remove', methods=['POST'])
+def remove_container():
+    data = AbortDict(request.get_json())
+
+    ids = data['ids']
+    ms = _peek_grpc(core.remove_container(ids))
+    q = Queue()
+
+    @with_appcontext
+    def _stream_producer():
+        for m in ms:
+            q.put(json.dumps(m, cls=JSONEncoder) + '\n')
+            if not m.success:
+                continue
+
+            Container.delete_by_container_id(m.id)
+        q.put(_eof)
+
+    t = Thread(target=_stream_producer)
+    t.daemon = True
+    t.start()
+
+    return Response(_stream_consumer(q), mimetype='application/json')
+
+
+@bp.route('/upgrade', methods=['POST'])
+def upgrade_container():
+    data = AbortDict(request.get_json())
+
+    ids = data['ids']
+    repo = data['repo']
+    sha = data['sha']
+
+    project_name = get_project_name(repo)
+    content = get_file_content(project_name, 'app.yaml', sha)
+    if not content:
+        abort(400, 'repo %s, %s does not have app.yaml in root directory' % (repo, sha))
+
+    specs = yaml.load(content)
+    appname = specs.get('appname', '')
+    release = Release.get_by_app_and_sha(appname, sha)
+    if not release:
+        abort(400, 'repo %s, %s does not have the right appname in app.yaml' % (repo, sha))
+
+    if not release.image:
+        abort(400, 'repo %s, %s has not been built yet' % (repo, sha))
+
+    ms = _peek_grpc(core.upgrade_container(ids, release.image))
+    q = Queue()
+
+    @with_appcontext
+    def _stream_producer():
+        for m in ms:
+            q.put(json.dumps(m, cls=JSONEncoder) + '\n')
+            if not m.success:
+                continue
+
+            old = Container.get_by_container_id(m.id)
+            if not old:
+                continue
+
+            c = Container.create(old.appname, sha, m.new_id, old.entrypoint, old.env, old.cpu_quota, old.podname, old.nodename)
+            if not c:
+                continue
+
+            old.delete()
+
+        q.put(_eof)
+
+    t = Thread(target=_stream_producer)
+    t.daemon = True
+    t.start()
+
+    return Response(_stream_consumer(q), mimetype='application/json')
--- citadel/api/v1/container.py
+++ citadel/api/v1/container.py
@@ -1,11 +1,7 @@
 # coding: utf-8
 from flask import abort
-from webargs import fields, ValidationError
-from webargs.flaskparser import use_args
 
-from citadel.ext import core
 from citadel.libs.view import create_api_blueprint
-from citadel.models.app import App, Release
 from citadel.models.container import Container
 
 
@@ -18,35 +14,3 @@ def get_container(container_id):
     if not c:
         abort(404, 'container `%s` not found' % container_id)
     return c
-
-
-def _get_app_by_name(appname):
-    app = App.get_by_name(appname)
-    if not app:
-        raise ValidationError('no valid address')
-    return app
-
-
-_deploy_args = {
-    'appname': fields.Function(required=True, deserialize=_get_app_by_name),
-    'podname': fields.Str(required=True),
-    'cpu_quota': fields.Int(missing=1),
-    'ncontainer': fields.Int(missing=1),
-    'sha': fields.Str(required=True),
-    'entrypoint': fields.Str(required=True),
-    'env': fields.Str(required=True),
-}
-
-
-@bp.route('/add', methods=['POST'])
-@use_args(_deploy_args)
-def create_container(args):
-    app = args['app']
-    appname, specs = app.specs, app.name
-    release = Release.get_by_app_and_sha(appname, args['sha'])
-    # TODO: unfinished
-    # TODO: networks 不知道怎么搞，是复用 eru-core 的 networks 部分吗？
-    # TODO: pod 要抄吗？之前 eru-core 的 deploy 需要搞那个 nshare，但是 corerpc
-    # 里边貌似不需要额
-    core.create_container(specs, appname, release.image, args['podname'],
-                          args['entrypoint'], args['cpu_quota'], args['ncontainer'], )
