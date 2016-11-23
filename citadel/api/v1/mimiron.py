f56fb1dee2c42435274f19532584c0e05979ecb6
tonic <tonicbupt@gmail.com>
2016-11-23 17:01:21 +0800

    add mimiron to citadel

--- citadel/api/v1/mimiron.py
+++ citadel/api/v1/mimiron.py
@@ -1,23 +1,15 @@
 # coding: utf-8
 
 from citadel.libs.view import create_api_blueprint
+from citadel.libs.mimiron import get_mimiron_containers_for_user
 from citadel.models.container import Container
-from citadel.ext import rds
 
 
 bp = create_api_blueprint('mimiron', __name__, 'mimiron')
 
+
 @bp.route('/container/<username>')
 def get_containers(username):
-    key = 'mimiron:{}:route'.format(username)
-    cids =  rds.hkeys(key)
-
-    res = []
-    for cid in cids:
-        info = Container.get_by_container_id(cid)
-        res.append({
-            'cid': cid,
-            'appname': info.appname,
-            'entrypoint': info.entrypoint
-        })
-    return res
+    ids = get_mimiron_containers_for_user(username)
+    containers = [Container.get_by_container_id(i) for i in ids]
+    return [{'cid': c.container_id, 'appname': c.appname, 'entrypoint': c.entrypoint} for c in containers if c]
