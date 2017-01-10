28dcdaa1c817dc5023652cbbea1e31e0dec26fe1
timfeirg <kkcocogogo@gmail.com>
2017-01-10 19:22:44 +0800

    get_all_containers API

--- citadel/api/v1/container.py
+++ citadel/api/v1/container.py
@@ -8,6 +8,12 @@ from citadel.models.container import Container
 bp = create_api_blueprint('container', __name__, 'container')
 
 
+@bp.route('/')
+def get_all_containers():
+    cs = Container.get_all()
+    return cs
+
+
 @bp.route('/<container_id>', methods=['GET'])
 def get_container(container_id):
     c = Container.get_by_container_id(container_id)
