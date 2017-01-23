5412785a8b914a15b8b530cca9ae6963f08737df
Zhang Ye <zhangye@ricebook.com>
2017-01-23 17:57:05 +0800

    add api for memcap

--- citadel/api/v1/pod.py
+++ citadel/api/v1/pod.py
@@ -3,6 +3,7 @@ from flask import abort, g, request
 
 from citadel.libs.datastructure import AbortDict
 from citadel.libs.view import create_api_blueprint
+from citadel.libs.memcap import get_node_memcap, sync_node_memcap
 from citadel.models.container import Container
 from citadel.rpc import get_core
 
@@ -76,3 +77,13 @@ def add_node(name):
         abort(400, 'cafile, certfile, keyfile must be either all empty or none empty')
 
     return get_core(g.zone).add_node(nodename, endpoint, pod.name, cafile, certfile, keyfile, public)
+
+
+@bp.route('/<name>/getmemcap', methods=['GET'])
+def get_memcap(name):
+    return get_node_memcap(g.zone, name)
+
+
+@bp.route('/<name>/syncmemcap', methods=['POST'])
+def sync_memcap(name):
+    return sync_node_memcap(g.zone, name)
