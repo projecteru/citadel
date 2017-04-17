e5c513e4159107d6a58a29912eb8ff31b9499b22
timfeirg <kkcocogogo@gmail.com>
2017-04-17 19:50:51 +0800

    [skip ci] token 没有清理干净

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -15,10 +15,6 @@ then
 EOF
 elif [ $deploy_mode == "prod" ]
 then
-  cat > ~/.corecli.json << EOF
-{"sso_url": "http://sso.ricebook.net", "citadel_url": "http://citadel.ricebook.net", "auth_token": "nHBKNpFBbRe5nRbuS2Z1ta7IkDQpcie0i4qRf09W5oBeWl5PcktCOykhBbT8vtDQ", "mimiron_url": "", "username": "liuyifu"}
-EOF
-  echo "corecli config has been set to citadel.ricebook.net"
   ssh c2-eru-1 << EOF
   sudo git --work-tree=/opt/citadel --git-dir=/opt/citadel/.git fetch --all --prune
   sudo git --work-tree=/opt/citadel --git-dir=/opt/citadel/.git reset --hard $remote/$branch
