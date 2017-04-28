83d4b3317338b7221bdce353d801291b5d66d515
timfeirg <kkcocogogo@gmail.com>
2017-04-28 14:38:01 +0800

    handle kibana query for elb rule with mount point

--- citadel/views/helper.py
+++ citadel/views/helper.py
@@ -144,10 +144,24 @@ def need_admin(f):
 
 
 def make_kibana_url(appname=None, ident=None, entrypoint=None, domain=None):
+    """
+    >>> make_kibana_url(domain='open.seriousapps.cn/cart')
+    'http://kibana.ricebook.net/app/kibana?#/discover?_g=%28%29&_a=%28columns%3A%21%28_source%29%2Cindex%3A%27rsyslog-nginx-%2A%27%2Cinterval%3Aauto%2Cquery%3A%28query_string%3A%28analyze_wildcard%3A%21t%2Cquery%3A%27host%3Aopen.seriousapps.cn+%26%26+uri%3A%5C%2Fcart%5C%2F%2A%27%29%29%2Csort%3A%21%28rsyslog_ts%2Cdesc%29%29'
+
+    >>> make_kibana_url(domain='notbot.intra.ricebook.net')
+    'http://kibana.ricebook.net/app/kibana?#/discover?_g=%28%29&_a=%28columns%3A%21%28_source%29%2Cindex%3A%27rsyslog-nginx-%2A%27%2Cinterval%3Aauto%2Cquery%3A%28query_string%3A%28analyze_wildcard%3A%21t%2Cquery%3A%27host%3Anotbot.intra.ricebook.net%27%29%29%2Csort%3A%21%28rsyslog_ts%2Cdesc%29%29'
+    """
     if domain:
+        domain = domain.rstrip('/')
+        if '/' in domain:
+            host, path = domain.split('/', 1)
+            query = 'host:{} && uri:\/{}\/*'.format(host, path.replace('/', '\/'))
+        else:
+            query = 'host:{}'.format(domain)
+
         params = {
             '_g': '()',
-            '_a': "(columns:!(_source),index:'rsyslog-nginx-*',interval:auto,query:(query_string:(analyze_wildcard:!t,query:'host:{}')),sort:!(rsyslog_ts,desc))".format(domain),
+            '_a': "(columns:!(_source),index:'rsyslog-nginx-*',interval:auto,query:(query_string:(analyze_wildcard:!t,query:'{}')),sort:!(rsyslog_ts,desc))".format(query),
         }
         return "http://kibana.ricebook.net/app/kibana?#/discover?" + urlencode(params)
     if not appname:
