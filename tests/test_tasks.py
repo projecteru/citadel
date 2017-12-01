e57c528d93bdac89b5778384b587e5bab30a1717
timfeirg <kkcocogogo@gmail.com>
2017-12-01 08:31:50 +0800

    improve test_build_image a bit

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,6 +1,7 @@
 import pytest
 
 from .prepare import core_online, default_appname, default_sha
+from citadel.models.app import Release
 from citadel.tasks import build_image
 
 
@@ -8,5 +9,11 @@ pytestmark = pytest.mark.skipif(not core_online, reason='one or more eru-core is
 
 
 def test_build_image(test_db, client):
+    release = Release.get_by_app_and_sha(default_appname, default_sha)
+    release.update_image('shit')
+    assert release.image == 'shit'
+
     image_tag = build_image(default_appname, default_sha)
-    assert '{}:{}'.format(default_appname, default_sha) in image_tag
+    release = Release.get_by_app_and_sha(default_appname, default_sha)
+    assert '{}:{}'.format(default_appname, default_sha) in release.image
+
