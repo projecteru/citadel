a11846916cda28b2db34b67172dde4a25f5b9b41
timfeirg <kkcocogogo@gmail.com>
2017-12-15 17:21:54 +0800

    remove sync_memcap related code, new eru-core doesn't have sync_memcap

--- citadel/api/pod.py
+++ citadel/api/pod.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import g, request, abort
 
-from citadel.ext import cache
 from citadel.libs.datastructure import AbortDict
-from citadel.libs.memcap import get_node_memcap, sync_node_memcap
 from citadel.libs.view import create_api_blueprint
 from citadel.models.container import Container
 from citadel.rpc.client import get_core
@@ -21,7 +19,6 @@ def _get_pod(name):
 
 
 @bp.route('/', methods=['GET'])
-@cache.cached(timeout=60 * 10)
 def get_all_pods():
     return get_core(g.zone).list_pods()
 
@@ -32,7 +29,6 @@ def get_pod(name):
 
 
 @bp.route('/<name>/nodes', methods=['GET'])
-@cache.cached(timeout=60 * 10)
 def get_pod_nodes(name):
     pod = _get_pod(name)
     return get_core(g.zone).get_pod_nodes(pod.name)
@@ -45,7 +41,6 @@ def get_pod_containers(name):
 
 
 @bp.route('/<name>/networks', methods=['GET'])
-@cache.cached(timeout=60 * 10)
 def get_pod_networks(name):
     pod = _get_pod(name)
     return get_core(g.zone).get_pod_networks(pod.name)
@@ -81,13 +76,3 @@ def add_node(name):
         abort(400, 'cafile, certfile, keyfile must be either all empty or none empty')
 
     return get_core(g.zone).add_node(nodename, endpoint, pod.name, cafile, certfile, keyfile, public)
-
-
-@bp.route('/<name>/getmemcap', methods=['GET'])
-def get_memcap(name):
-    return get_node_memcap(g.zone, name)
-
-
-@bp.route('/<name>/syncmemcap', methods=['POST'])
-def sync_memcap(name):
-    return sync_node_memcap(g.zone, name)
