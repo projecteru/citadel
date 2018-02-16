c76d4de60f4564540df1422192b6669e5120f3d0
timfeirg <kkcocogogo@gmail.com>
2018-02-16 19:18:11 +0800

    re-write Specs using box
    * refactor Specs to match eru-core's types/specs.go
    * remove special attributes from ActionError

--- citadel/models/specs.py
+++ citadel/models/specs.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import yaml
+from box import Box
 from crontab import CronTab
 from humanfriendly import InvalidTimespan, parse_timespan, parse_size
 from marshmallow import fields, validates_schema, ValidationError, post_load
 from numbers import Number
 
-from citadel.libs.jsonutils import Jsonized
-from citadel.libs.utils import memoize
 from citadel.models.base import StrictSchema
-
-
-def validate_protocol(s):
-    if s not in {'http', 'tcp'}:
-        raise ValidationError('ELB port should talk tcp or http')
 
 
 def validate_port(n):
@@ -66,34 +59,6 @@ def parse_builds(dic):
     return dic
 
 
-def parse_single_port(port_string):
-    if isinstance(port_string, Number):
-        data = {'port': str(port_string), 'protocol': 'tcp'}
-    else:
-        parts = port_string.split('/')
-        try:
-            port = int(parts[0])
-        except ValueError:
-            raise ValidationError('Bad port: {}'.format(port_string))
-        if len(parts) == 2:
-            protocol = parts[1]
-        elif len(parts) == 1:
-            protocol = 'tcp'
-        else:
-            raise ValidationError('Multiple slashes in port: {}'.format(port_string))
-        data = {'port': port, 'protocol': protocol}
-
-    unmarshal_result = PortSchema().load(data)
-    errors = unmarshal_result.errors
-    if errors:
-        raise ValidationError(str(errors))
-    return unmarshal_result.data
-
-
-def parse_port_list(port_list):
-    return [parse_single_port(s) for s in port_list]
-
-
 def parse_memory(s):
     return parse_size(s, binary=True) if isinstance(s, str) else s
 
@@ -138,8 +103,7 @@ def better_parse_timespan(s):
 
 def parse_cron_line(s):
     try:
-        l = s.split()
-        cron_part = ' '.join(l[:5])
+        cron_part = ' '.join(s.split()[:5])
         crontab = CronTab(cron_part)
         cmd = s.replace(cron_part, '').strip()
     except IndexError as e:
@@ -150,18 +114,6 @@ def parse_cron_line(s):
 def parse_crontab(cron_list):
     cron_settings = [parse_cron_line(s) for s in cron_list]
     return cron_settings
-
-
-class PortSchema(StrictSchema):
-    protocol = fields.Str(validate=validate_protocol, missing='tcp')
-    port = fields.Int(validate=validate_port, required=True)
-
-
-class Port(Jsonized):
-    def __init__(self, protocol=None, port=None, _raw=None):
-        self.protocol = protocol
-        self.port = port
-        self._raw = _raw
 
 
 class BuildSchema(StrictSchema):
@@ -182,8 +134,8 @@ build_schema = BuildSchema()
 
 class HealthCheckSchema(StrictSchema):
     tcp_ports = fields.List(fields.Int(validate=validate_port), missing=[])
-    http_port = fields.Int(validate=validate_port, missing='')
-    http_url = fields.Str(missing='')
+    http_port = fields.Int(validate=validate_port)
+    http_url = fields.Str()
     http_code = fields.Int(validate=validate_http_code)
 
     @post_load
@@ -199,49 +151,36 @@ class HealthCheckSchema(StrictSchema):
             raise ValidationError('If you plan to use HTTP health check, you must define (at least) http_port, http_url')
 
 
-class HealthCheck(Jsonized):
-    def __init__(self, tcp_ports=None, http_port=None, http_url=None, http_code=None, _raw=None):
-        self.tcp_ports = tcp_ports
-        self.http_port = http_port
-        self.http_url = http_url
-        self.http_code = http_code
-        self._raw = _raw
+class HookSchema(StrictSchema):
+    after_start = fields.List(fields.Str(), missing=[])
+    before_stop = fields.List(fields.Str(), missing=[])
+    force = fields.Bool()
 
 
 class EntrypointSchema(StrictSchema):
-    cmd = fields.Str(attribute='command', required=True)
+    command = fields.Str(required=True, load_from='cmd')
     image = fields.Str()
-    ports = fields.Function(deserialize=parse_port_list, missing=[])
+    publish = fields.List(fields.Str(), missing=[], load_from='ports')
     network_mode = fields.Str()
     restart = fields.Str(validate=validate_restart)
     healthcheck = fields.Nested(HealthCheckSchema)
     privileged = fields.Bool(missing=False)
     log_config = fields.Str(validate=validate_log_config)
     working_dir = fields.Str(load_from='dir')
-    after_start = fields.Str()
-    before_stop = fields.Str()
+    hook = fields.Nested(HookSchema)
     backup_path = fields.List(fields.Str(), missing=[])
 
+    @post_load
+    def finalize(self, data):
+        # if there's anything to be published to ELB, give em some free tcp
+        # healthcheck, if there's no healthcheck defined at all
+        publish = data.get('publish')
+        if publish and not data.get('healthcheck'):
+            data['healthcheck'] = {
+                'tcp_ports': publish,
+            }
 
-class Entrypoint(Jsonized):
-    def __init__(self, command=None, image=None, ports=None, network_mode=None,
-                 restart=None, healthcheck={}, hosts=None, privileged=None,
-                 log_config=None, working_dir=None, after_start=None,
-                 before_stop=None, backup_path=None, _raw=None):
-        self.command = command
-        self.image = image
-        self.ports = [Port(_raw=data, **data) for data in ports]
-        self.network_mode = network_mode
-        self.restart = restart
-        self.healthcheck = HealthCheck(_raw=healthcheck, **healthcheck)
-        self.hosts = hosts
-        self.privileged = privileged
-        self.log_config = log_config
-        self.working_dir = working_dir
-        self.after_start = after_start
-        self.before_stop = before_stop
-        self.backup_path = backup_path
-        self._raw = _raw
+        return data
 
 
 entrypoint_schema = EntrypointSchema()
@@ -263,90 +202,43 @@ class SpecsSchema(StrictSchema):
     crontab = fields.Function(deserialize=parse_crontab, missing=[])
 
     @post_load
-    def fix_defaults(self, data):
+    def finalize(self, data):
+        """add defaults to fields, and then construct a Box"""
         if 'name' not in data:
             data['name'] = data['appname']
 
-        if 'base' in data:
-            default_build_base = data['base']
+        default_build_base = data.get('base')
+        if default_build_base:
             for build in data['builds'].values():
                 if 'base' not in build:
                     build['base'] = default_build_base
 
-        for _, entrypoint in data['entrypoints'].items():
+        for entrypoint in data['entrypoints'].values():
             # set default working_dir to app's home
             if not entrypoint.get('working_dir'):
                 entrypoint['working_dir'] = '/home/{}'.format(data['appname'])
 
-            # if there's anything to be published to ELB, give em some free tcp
-            # healthcheck, if there's no healthcheck defined at all
-            publish_ports = entrypoint.get('ports')
-            if publish_ports and not entrypoint.get('healthcheck'):
-                entrypoint['healthcheck'] = {
-                    'tcp_ports': [str(p['port']) for p in publish_ports],
-                }
+        return Box(data, conversion_box=False, default_box=True,
+                   default_box_attr=None, frozen_box=True)
 
-    @validates_schema(pass_original=True)
-    def validate_misc(self, data, original_data):
+    @validates_schema
+    def validate_misc(self, data):
         # check raw related fields
-        raw = False if data.get('stages') else True
+        stages = data.get('stages')
+        builds = data.get('builds')
+        if set(stages) != set(builds):
+            raise ValidationError('stages inconsistent with builds: {} vs {}'.format(stages, list(builds.keys())))
+
+        raw = False if stages else True
         if not raw and data.get('container_user'):
             raise ValidationError('cannot specify container_user because this release is not raw')
         if raw and not data.get('base'):
             raise ValidationError('this is a raw release, must specify base')
 
         if not data.get('base'):
-            for build in data['builds'].values():
+            for build in builds.values():
                 if not build.get('base'):
                     raise ValidationError('either use a global base image as default build base, or specify base in each build stage')
 
 
-class Specs(Jsonized):
-
-    exclude_from_dump = ['crontab']
-
-    def __init__(self, appname=None, name=None, entrypoints={}, dns=None,
-                 hosts=None, stages=None, container_user=None, builds={},
-                 volumes=None, base=None, subscribers=None,
-                 erection_timeout=None, crontab=None, _raw=None):
-        self.appname = appname
-        self.name = name
-        self.entrypoints = {entrypoint_name: Entrypoint(_raw=data, **data) for entrypoint_name, data in entrypoints.items()}
-        self.dns = dns
-        self.hosts = hosts
-        self.stages = stages
-        self.container_user = container_user
-        self.builds = builds
-        self.volumes = volumes
-        self.base = base
-        self.subscribers = subscribers
-        self.erection_timeout = erection_timeout
-        self.crontab = crontab
-        self._raw = _raw
-        for field_name in self.exclude_from_dump:
-            del _raw[field_name]
-
-    @classmethod
-    def validate(cls, s):
-        dic = yaml.load(s)
-        unmarshal_result = SpecsSchema().load(dic)
-        errors = unmarshal_result.errors
-        if errors:
-            raise ValidationError(str(errors))
-        # validate build related clause
-        declared_stages = set(dic['stages'])
-        actual_build_stages = set(dic['builds'])
-        if declared_stages != actual_build_stages:
-            raise ValidationError('stages inconsistent with builds: {} vs {}'.format(declared_stages, actual_build_stages))
-
-    @classmethod
-    def from_dict(cls, dic):
-        unmarshal_result = SpecsSchema().load(dic)
-        data = unmarshal_result.data
-        return cls(_raw=data, **data)
-
-    @classmethod
-    @memoize
-    def from_string(cls, s):
-        dic = yaml.load(s)
-        return cls.from_dict(dic)
+specs_schema = SpecsSchema()
